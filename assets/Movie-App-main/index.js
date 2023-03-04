e0aa4cf169496d53a0a84ded1ba170f0b51d2b07
paulthomas58 <98998783+paulthomas58@users.noreply.github.com>
2023-03-04 13:25:12 -0500

    Update index.js

--- assets/Movie-App-main/index.js
+++ assets/Movie-App-main/index.js
@@ -6,7 +6,7 @@ let result = document.getElementById("result");
 
 let getMovie = () => {
     let movieName = movieNameRef.value;
-    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
+    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=e89c8f0c`;
     //if input field is empty
 
     if (movieName.length <= 0) {
